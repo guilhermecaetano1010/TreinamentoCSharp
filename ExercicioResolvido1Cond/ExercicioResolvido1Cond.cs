eb2927843b996874dcff5a23edaf6c836410a15c
GUILHERME CAETANO SILVA <gsilva@websupply.com.br>
2023-09-12 09:20:56 -0300

    Exercício resolvido 01 condicional

---
+++ ExercicioResolvido1Cond/ExercicioResolvido1Cond.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+namespace ExercicioResolvido1Cond
+{
+    public class ExercicioResolvido1Cond
+    {
+        static void Main(string[] args){
+
+            double nota1, nota2, soma;
+
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        
+            soma = nota1+nota2;
+
+            Console.WriteLine("NOTA FINAL = "+soma.ToString("F1", CultureInfo.InvariantCulture));
+
+            if(soma < 60.0){
+                Console.WriteLine("REPROVADO");
+            }
+        }
+    }
+}
