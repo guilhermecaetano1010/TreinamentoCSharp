77a4c5704af42f3d4b4cb414e0349f832db73a4c
guilhermecaetano1010 <gsilva@websupply.com.br>
2023-09-12 10:23:58 -0300

    ExercicioResolvido3Cond

---
+++ ExercicioResolvido3Cond/ExercicioResolvido3Cond.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+namespace ExercicioResolvido3Cond
+{
+    public class ExercicioResolvido3Cond
+    {
+        static void Main(string[] args){
+            int x,y,z;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+            z = int.Parse(vet[2]);
+
+            if(x < y || x )
+        }
+    }
+}
