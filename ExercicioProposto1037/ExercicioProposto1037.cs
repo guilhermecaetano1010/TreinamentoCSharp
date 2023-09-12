03c1c3e4baeacee795ed7626fde1d5c57b11d185
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-12 12:01:43 -0300

    Adicionando o exercício 1037

---
+++ ExercicioProposto1037/ExercicioProposto1037.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+
+namespace ExercicioProposto1037
+{
+    public class ExercicioProposto1037
+    {
+        static void Main(string[] args){
+
+            double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero < 0.0 || numero > 100.0) {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if (numero <= 25.0) {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (numero <= 50.0) {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (numero <= 75.0) {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+        }
+    }
+}
+/*Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+
+O símbolo ( representa "maior que". Por exemplo:
+[0,25]  indica valores entre 0 e 25.0000, inclusive eles.
+(25,50] indica valores maiores que 25 Ex: 25.00001 até o valor 50.0000000*/
