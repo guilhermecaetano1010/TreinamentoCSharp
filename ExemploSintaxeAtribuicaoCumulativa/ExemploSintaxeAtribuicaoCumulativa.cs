10a4a4df1e274d25fe10738f0c81e8cf7af1618d
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-12 10:57:52 -0300

    Adicionando o exemplo de atribuição cumulativa

---
+++ ExemploSintaxeAtribuicaoCumulativa/ExemploSintaxeAtribuicaoCumulativa.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+
+namespace ExemploSintaxeAtribuicaoCumulativa
+{
+    public class ExemploSintaxeAtribuicaoCumulativa
+    {
+        static void Main(string[] args){
+
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100) {
+                conta += (minutos - 100) * 2.0;
+            }
+
+            Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
