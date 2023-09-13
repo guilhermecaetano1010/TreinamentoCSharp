75f055a817f3473834f3e0f170e5f181bff1561c
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-13 15:08:26 -0300

    Adicionando uma classe

---
+++ ExerciciosEstruturasRepetitivas/ExercicioResolvido2Repet/ExercicioResolvido2Repet.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExercicioResolvido2Repet
+{
+    public class ExercicioResolvido2Repet
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
