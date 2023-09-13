3824aba5ddb328e221852daed0b76a868155a4f6
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-13 11:26:29 -0300

    Adicionando comentário

--- ExercicioProposto1048/ExercicioProposto1048.cs
+++ ExercicioProposto1048/ExercicioProposto1048.cs
@@ -35,3 +35,20 @@ namespace ExercicioProposto1048
         } 
     }
 }
+/*A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
+
+
+Salário	Percentual de Reajuste
+0 - 400.00
+400.01 - 800.00
+800.01 - 1200.00
+1200.01 - 2000.00
+Acima de 2000.00
+
+15%
+12%
+10%
+7%
+4%
+
+Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.*/
