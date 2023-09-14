1d6620e57cfff0a6c2e7fe6665208d7c5ca92ead
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-14 08:29:31 -0300

    Adicionando o exercício resolvido 03

--- ExerciciosEstruturasRepetitivas/ExercicioResolvido2Repet/ExercicioResolvido2Repet.cs
+++ ExerciciosEstruturasRepetitivas/ExercicioResolvido2Repet/ExercicioResolvido2Repet.cs
@@ -1,12 +1,33 @@
 using System;
-
+using System.Globalization;
 namespace ExercicioResolvido2Repet
 {
     public class ExercicioResolvido2Repet
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double idade, soma, media;
+            int cont;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = 0.0;
+            cont= 0;
+            
+            while (idade >= 0) {
+                soma = soma + idade;
+                cont = cont + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (cont == 0) {
+                Console.WriteLine("Impossível calcular");
+            }
+
+            else{
+                media = soma / cont;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
         }
     }
 }
