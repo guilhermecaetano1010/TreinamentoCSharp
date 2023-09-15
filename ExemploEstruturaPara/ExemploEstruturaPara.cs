a1df9a1388190d424990c054ed382159dcf77129
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-15 09:11:55 -0300

    Adicionando o exemplo de estrutura Para

---
+++ ExemploEstruturaPara/ExemploEstruturaPara.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+namespace ExemploEstruturaPara
+{
+    public class ExemploEstruturaPara
+    {
+         static void Main(string[] args) {
+
+               int N, x, soma;
+
+               N = int.Parse(Console.ReadLine());
+
+               soma = 0;
+               for (int i = 1; i <= N; i++) {
+
+               x = int.Parse(Console.ReadLine());
+               soma += x;
+
+               }
+               Console.WriteLine(soma);
+        }
+    }
+}
