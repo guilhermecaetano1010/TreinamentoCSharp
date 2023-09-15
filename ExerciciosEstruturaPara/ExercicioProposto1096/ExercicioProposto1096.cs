bdd6e5e3c6f84637bb46457b860dc711a2f12b1f
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-15 12:49:16 -0300

    Modificando o exercício

--- ExerciciosEstruturaPara/ExercicioProposto1096/ExercicioProposto1096.cs
+++ ExerciciosEstruturaPara/ExercicioProposto1096/ExercicioProposto1096.cs
@@ -6,16 +6,12 @@ namespace ExercicioProposto1096
     {
         static void Main(string[] args)
         {
-          int i = 1;
-            if (i % 2 == 1)
-            {
-                for (i = 1; i <= 9; i = i + 2)
+                for (int i = 1; i <= 9; i = i + 2)
                 {
                     for (int j = 7; j >= 5; j--) {
                         Console.WriteLine("I=" + i + " " + "J=" + j);
                     }
                 }
-            }
 
         }
     }
