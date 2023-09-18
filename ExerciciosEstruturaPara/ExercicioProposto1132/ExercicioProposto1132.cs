309b7c2921536fee3495f85c89fa8214af02b17b
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-18 09:33:55 -0300

    Adicionando o exercício 1132

--- ExerciciosEstruturaPara/ExercicioProposto1132/ExercicioProposto1132.cs
+++ ExerciciosEstruturaPara/ExercicioProposto1132/ExercicioProposto1132.cs
@@ -5,7 +5,26 @@ namespace ExercicioProposto1132
 {
     public class ExercicioProposto1132 {
         static void Main(string[] args) {
-             int X = int.Parse(Console.ReadLine)   
+             int X = int.Parse(Console.ReadLine());
+             int Y = int.Parse(Console.ReadLine());
+             int soma = 0;
+            if(Y > X){
+             for (int i = X; i <= Y; i++) {
+                if(i % 13 != 0){
+                soma = soma + i;
+                }
+             }  
+            }
+            else{
+             for (int j = Y; j <= X; j++) {
+                if(j % 13 != 0){
+                soma = soma + j;
+                }
+            }
+            }  
+             Console.WriteLine(soma);
         }
     }
 }
+/**/
+
