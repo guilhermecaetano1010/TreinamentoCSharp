233c3caf73b8ee00d2247316ea3eb04e7398cdf2
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-18 09:05:47 -0300

    Teste

---
+++ ExerciciosEstruturaPara/ExercicioProposto1132/ExercicioProposto1132.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+namespace ExercicioProposto1132
+{
+    public class ExercicioProposto1132 {
+        static void Main(string[] args) {
+             int X = int.Parse(Console.ReadLine)   
+        }
+    }
+}
