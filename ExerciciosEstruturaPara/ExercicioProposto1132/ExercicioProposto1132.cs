21923c8a334a190e62cf8851511a6383a7ff6bd3
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-18 09:57:51 -0300

    Alterando o exercício 1132

--- ExerciciosEstruturaPara/ExercicioProposto1132/ExercicioProposto1132.cs
+++ ExerciciosEstruturaPara/ExercicioProposto1132/ExercicioProposto1132.cs
@@ -26,5 +26,5 @@ namespace ExercicioProposto1132
         }
     }
 }
-/**/
+/*Escreva um algoritmo que leia 2 valores inteiros X e Y calcule a soma dos números que não são múltiplos de 13 entre X e Y, incluindo ambos*/
 
