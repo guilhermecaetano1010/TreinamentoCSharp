176421e3e12a2a1a2dca9c7d87cc450bdcc985a4
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-09-19 08:44:40 -0300

    Adicionando o exemplo de vetores

---
+++ ExemploVetores/ExemploVetores.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+
+namespace ExemploVetores
+{
+    public class ExemploVetores {
+       static void Main(string[] args) {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            for (int i = 0; i < N; i++) {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+       } 
+    }
+}
