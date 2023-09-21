e6a831900abf0d42095bcfa49b7e309468478e37
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-21 10:17:45 -0300

    Adicionando o exercício proposto 2

---
+++ ExerciciosVetores/ExercicioProposto2/ExercicioProposto2.cs
@@ -0,0 +1,35 @@
+using System;
+
+
+namespace ExercicioProposto2
+{
+    public class ExercicioProposto2
+    {
+        static void Main(string[] args) {
+                        int N = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++) {
+                vet[i] = int.Parse(valores[i]);
+            }
+
+            for (int i = 0; i < N; i++) {
+                if (vet[i] % 2 == 0) {
+                    Console.Write(vet[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            int quantidadeDePares = 0;
+            for (int i = 0; i < N; i++) {
+                if (vet[i] % 2 == 0) {
+                    quantidadeDePares++;
+                }
+            }
+            Console.WriteLine(quantidadeDePares);
+        }
+    }
+}
