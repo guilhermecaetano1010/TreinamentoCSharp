0554c82e1223407d333d13f04c026cc23009bbb8
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-21 10:18:55 -0300

    Modificando o exercício proposto 2

--- ExerciciosVetores/ExercicioProposto2/ExercicioProposto2.cs
+++ ExerciciosVetores/ExercicioProposto2/ExercicioProposto2.cs
@@ -33,3 +33,6 @@ namespace ExercicioProposto2
         }
     }
 }
+/*Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na tela:
+- todos os números pares
+- a quantidade de números pares*/
