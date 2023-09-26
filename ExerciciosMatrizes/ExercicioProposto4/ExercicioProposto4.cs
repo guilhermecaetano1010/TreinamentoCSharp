b469e4be5b019a14dcc8033abb72a22cac84be37
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-26 16:38:40 -0300

    Adicionando o exercício proposto 4 de matrizes

---
+++ ExerciciosMatrizes/ExercicioProposto4/ExercicioProposto4.cs
@@ -0,0 +1,31 @@
+using System;
+
+
+namespace ExercicioProposto4
+{
+    public class ExercicioProposto4
+    {
+            static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[N,N];
+
+            for (int i = 0; i < N; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    mat[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            int soma = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) { 
+                    soma = soma + mat[i, j];
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
+/*Ler um inteiro N e uma matriz quadrada de ordem N. Mostrar a soma dos elementos acima da diagonal principal.*/
